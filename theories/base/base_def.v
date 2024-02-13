91dc3089012ce7b7f90ddf4aaa7851ea20277893
DmxLarchey <larchey@gmail.com>
2024-02-13 10:18:57 +0100

    more explanations about the switch

--- theories/base/base_def.v
+++ theories/base/base_def.v
@@ -7,22 +7,46 @@
 (*        Mozilla Public License Version 2.0, MPL-2.0         *)
 (**************************************************************)
 
-(** This is a global switch for the whole code to be developed
-    with either
+(** This implements a global switch for the whole library with either
 
      a) impredicative and non-informative af predicates, ie Base := Prop
      b) predicative and informative af predicates, ie Base := Type
 
-    Comment out exactly one between base_prop or bas_type below
+    One can choose between a or b in the base_implem file which
+    is a copy of either ../../implem/prop.v or ../../implem/type.v
+    selected at compile time.
+
+    Notice that the installed libraries:
+    - KruskalAfProp sets Base := Prop
+    - KruskalAfType sets Base := Type
+    and the uniform *parsing only* notations below
+    for "first order syntax".
   *)
 
 Require Export base_notations base_implem.
 
 Import Base_Sums_Products.
 
+(* We have the following correspondance depending on Base := Prop or Type
+
+      +------+-----------+---------+----------+--------------+
+      | Base |   ⊥ₜ      |   ∨ₜ    |   ∧ₜ     |   ∃ₜ         |
+      +------+-----------+---------+----------+--------------+
+      | Prop | False     | ∨ / or  | ∧ / and  | ∃ / ex       |
+      | Type | Empty_set | + / sum | * / prod | { & } / sigT |
+      +------+-----------+---------+----------+--------------+
+
+   So the notation covers both cases with a common syntax
+*)
+
 #[global] Notation "⊥ₜ" := Absurd (only parsing) : type_scope.
 #[global] Notation "x ∨ₜ y" := (NonDepSum x y) (only parsing) : type_scope.
 #[global] Notation "x ∧ₜ y" := (NonDepProd x y) (only parsing) : type_scope.
-#[global] Notation "P ⇄ₜ Q" := ((P -> Q) ∧ₜ (Q -> P))%type (only parsing) : type_scope.
 #[global] Notation "∃ₜ x .. y , p" := (DepSum (fun x => .. (DepSum (fun y => p)) ..)) (only parsing) : type_scope.
+
+(* Notice that one CANNOT rewrite with ⇄ₜ when Base := Type so we choose
+   the denotation ⇄ₜ for informative equivalence instead of ↔ₜ which could 
+   otherwise be seen as more "natural". *)
+
+#[global] Notation "P ⇄ₜ Q" := ((P -> Q) ∧ₜ (Q -> P))%type (only parsing) : type_scope.
 
