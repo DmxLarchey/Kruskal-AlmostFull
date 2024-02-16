d8a809f3f20edd129da5f682330ee8c382220934
Dominique Larchey-Wendling <larchey@gmail.com>
2024-02-16 14:46:03 +0100

    cleanup the import file almost_full.v

--- theories/af/almost_full.v
+++ theories/af/almost_full.v
@@ -7,11 +7,13 @@
 (*        Mozilla Public License Version 2.0, MPL-2.0         *)
 (**************************************************************)
 
-Require Export (* list.list_fan *)
-               af.lift af.pfx_rev
+Require Export af.lift
+               af.pfx_rev
                af.good
-               af.af af.bar (* af.fan *)
-               af.af_eq af.af_good af.af_bar
+               af.af af.bar
+               af.af_eq 
+               af.af_good
+               af.af_bar
                af.af_tools
                af.af_wf.
 
